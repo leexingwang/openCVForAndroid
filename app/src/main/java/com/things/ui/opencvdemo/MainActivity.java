509493c70505161b7443cdc29926753b2310f74c
leexingwang <1163654820@qq.com>
2018-10-31 22:03:43 +0800

    qq

--- app/src/main/java/com/things/ui/opencvdemo/MainActivity.java
+++ app/src/main/java/com/things/ui/opencvdemo/MainActivity.java
@@ -85,9 +85,9 @@ public class MainActivity extends AppCompatActivity {
 //                oneByOne(mat, channels, width, height);
 //                allOnce(mat, channels, width, height);
 //                matSplit(mat, channels, width, height);
-                matBinarization(mat, channels, width, height);
+//                matBinarization(mat, channels, width, height);
 //                MatAddSubMultDiv(mat, width, height);
-//                matLight(mat, width, height);
+                matLight(mat, width, height);
 
             }
 
